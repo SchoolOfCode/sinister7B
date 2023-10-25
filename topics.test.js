062bc0ca0658236aabe75e4c729cb55298c27873
Patryk Hirny <pathirny@googlemail.com>
2023-10-25 12:16:00 +0100

    started testing

--- topics.test.js
+++ topics.test.js
@@ -10,8 +10,16 @@ import { resetDatabase } from "./Database/scripts/reset-database.js";
 // Test endpoint: getTopics
 test("GET /topics works", async function () {
   //call the reset function
-  await resetDatabase([
-    { task: "Walk the dog", completion_date: "1999-01-08" },
-    { task: "Feed the computer", completion_date: "2015-01-10" },
-]);
+  await resetDatabase();
+
+  // call the request and pass in the express app
+  const response = await request(app).get("/topics");
+
+  // store the response body in a responseBody variable
+  const responseBody = response.body;
+
+  // check that the data matches what is expected
+  responseBody.payload.forEach((responseBody) => {
+    expect(responseBody.id).toBeTypeOf("number");
+  });
 });
