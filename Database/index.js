1d4daf4bbc0efba91292f4b9a39f5d83c7f3aea6
Siyu Duan <siyusiyuduan@hotmail.com>
2023-10-25 10:22:03 +0100

    finished the index.js / pool

--- Database/index.js
+++ Database/index.js
@@ -5,3 +5,14 @@ import pg from "pg";
 const connectionString = process.env.DB_CONNECTION;
 
 // check if the connection string is not defined, if so throw an error
+if (!connectionString) {
+    throw new Error(
+        "No DE_CONNECTION defined. Did you load in your env variables?"
+    );
+}
+
+// Export a new instance of pg.pool
+export const pool = new pg.Pool({
+    // Pass the connection string to the pool
+    connectionString,
+});
