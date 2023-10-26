dcfdd3a4a968807aa1fd795a01756271d8d32965
Patryk Hirny <pathirny@googlemail.com>
2023-10-26 11:44:20 +0100

    fixed cors issue

--- app.js
+++ app.js
@@ -5,27 +5,26 @@ import express from "express";
 import morgan from "morgan";
 
 //import routes from routes handler files???
-import {topicsRoutes} from "./routes/topicRoutes.js";
+import { topicsRoutes } from "./routes/topicRoutes.js";
 
 //export const app will be set as default
-const app = express ();
-
+const app = express();
+//import cors from cors
+import cors from "cors";
 //use app (morgan,express.json, ???.Routes, )
-
-app.use (morgan("dev"));
-app.use (express.json());
-app.use ("/topics", topicsRoutes);
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/topics", topicsRoutes);
 
 //    error handling
 // Error handler for incorrect filepath
 app.use(function (_req, res, _next) {
-    res.status(404).json({
-      success: false,
-      error:
-        "We couldn't find what you were looking for 😞. Did you send the request to the right path?",
-    });
+  res.status(404).json({
+    success: false,
+    error:
+      "We couldn't find what you were looking for 😞. Did you send the request to the right path?",
   });
-  
-  export default app;
- 
+});
 
+export default app;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 //import app from app.js
 import app from "./app.js";
 //setting the port to the .env or 7777 if not found
-const port=process.env.PORT ?? 7777
+const port = process.env.PORT ?? 7777;
+
 //sett up app.listen to the port
 app.listen(port, () => {
-    console.log(`Server is now listening on http://localhost:${port}`)
+  console.log(`Server is now listening on http://localhost:${port}`);
 });
